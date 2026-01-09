7985fda124e0db5902d35b77453f548cb1dd95db
Adam-445 <Adamch012016@outlook.com>
2026-01-09 11:28:09 +0100

    feat(ast): Add String() method to statements to help with debugging and testing

--- ast/ast.go
+++ ast/ast.go
@@ -2,10 +2,15 @@
 // AKA the representation of a program in memorry so a computer understands it
 package ast
 
-import "github.com/Adam-445/go-interpreter/token"
+import (
+	"bytes"
+
+	"github.com/Adam-445/go-interpreter/token"
+)
 
 type Node interface {
 	TokenLiteral() string // Every AST node must be able to tell what token it came from
+	String() string       // Helper method for debugging and comparison between nodes
 }
 
 type Statement interface {
@@ -30,6 +35,14 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name  *Identifier
@@ -38,6 +51,17 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token // the token.IDENT token
@@ -46,6 +70,9 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string {
+	return i.Value
+}
 
 type ReturnStatement struct {
 	Token       token.Token // return token.RETURN token
@@ -54,6 +81,15 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 type ExpressionStatement struct {
 	Token      token.Token // The first token of the expression
@@ -62,3 +98,9 @@ type ExpressionStatement struct {
 
 func (es *ExpressionStatement) statementNode()       {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
